70e545a12a05ef8f5d43cb57698118f6aebb9d36
Daly <41022166+daly64@users.noreply.github.com>
2024-08-21 15:37:26 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    TodosModule,
+    // MongooseModule.forRoot('mongodb://localhost:27017/todo-api'),
+    MongooseModule.forRoot(
+      'mongodb+srv://daly:123123123@cluster0.tunc1.mongodb.net/todo-api',
+    ),
+  ],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { Todo } from './schemas/todo.schema';
+
+@Controller()
+export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
+  @Get()
+  findAll(): Promise<Todo[]> {
+    return this.todosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Todo> {
+    return this.todosService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createTodoDto: Partial<Todo>): Promise<Todo> {
+    return this.todosService.create(createTodoDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateTodoDto: Partial<Todo>,
+  ): Promise<Todo> {
+    return this.todosService.update(id, updateTodoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<any> {
+    return this.todosService.remove(id);
+  }
+}
